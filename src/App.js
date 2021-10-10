49bcc83f5f9e230afe73a43747b634cc0b9e1040
Rifaq.Shaikh <rifaq.shaikh@extentia.com>
2021-10-10 16:17:39 +0530

    aws-config file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Banker from "./components/Banker/banker";
 import Amplify from "aws-amplify";
-import awsconfig from "./aws-exports";
+import awsconfig from "./aws-config";
 import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(awsconfig);
