2d32b673f887fb8c9a6d24ec9217a90db60cb695
Rifaq.Shaikh <rifaq.shaikh@extentia.com>
2021-10-11 21:25:48 +0530

    added access token

--- src/components/Banker/banker.js
+++ src/components/Banker/banker.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import DropdownComponent from "../Dropdown/dropdown";
 import axios from "axios";
 import { API_ENDPOINT } from "../../App";
+import { APP_CLIENT_ID } from "../../App";
 import "./banker.css";
 
 function Banker() {
@@ -9,8 +10,15 @@ function Banker() {
 	const [loanData, setLoanData] = useState([]);
 	let currentStatus = "";
 
-	axios.defaults.headers.common["Authorization"] =
-		"eyJraWQiOiJrSURWK2lTS0RvMU5mMGJHbjhSUlpiMnNIdE1jYVJmS2JZMDB6eWVXM29rPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI5ZWIzYzZlNy1lMjM5LTRlZTQtYWUxOC1mMjNiNmU0NzhkN2UiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIHBob25lIG9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNjMzOTUxMjgxLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGgtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0aC0xX2hFamF6OGdxcyIsImV4cCI6MTYzNDAzNzY4MSwiaWF0IjoxNjMzOTUxMjgxLCJ2ZXJzaW9uIjoyLCJqdGkiOiI5YzUzMjVmNi1jODZjLTRmZGItODc5My01OGJjZjkzZTQ5MDEiLCJjbGllbnRfaWQiOiIzNHNvNGplcjQ0dTJwMzlmZmpxYmxuN2N2dCIsInVzZXJuYW1lIjoicHJhdmluIn0.p2ZcFiSffczFijC0prB79KC_H_MoJnI67Vsb8WIRQk2okzLLHjBFkmNdY6ssd6OJ5NIN2nvXf90COQuillmnYvGj-z7qdp-ZvQOSNBrkS_kXUQnN-lDqTToF00ZgzjxVWOoMFqD93uVufpwNLDp_taMeAnyd4ZkvxSdBjV6D3Twi-yiCqs4IJsfjXVYJozt32ONBi0k4Ghc2F66DWtlQRSLe6g7W7xHJ7Lnd9jAWfg4kBn33t4QCLDmrbzWTjPIaIEXJlDdjkohe2aDGt0G7DilwP7508Sk19CNCSxPs75sOtWvH7mVjKz9cXApjyf-d4vFtSLIRrtsuiRlnGKpXLg";
+	const userName = localStorage.getItem(
+		`CognitoIdentityServiceProvider.${APP_CLIENT_ID}.LastAuthUser`
+	);
+
+	const accessToken = localStorage.getItem(
+		`CognitoIdentityServiceProvider.${APP_CLIENT_ID}.${userName}.accessToken`
+	);
+
+	axios.defaults.headers.common["Authorization"] = accessToken;
 	useEffect(() => {
 		axios
 			.get(`${API_ENDPOINT}/getloanstatustypebanker/loanstatus`)
