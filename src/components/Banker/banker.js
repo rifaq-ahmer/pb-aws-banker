a4ced385d3968e93e9b12e8462a05c893ed6bc14
Rifaq.Shaikh <rifaq.shaikh@extentia.com>
2021-10-11 16:42:29 +0530

    Added Access Token

--- src/components/Banker/banker.js
+++ src/components/Banker/banker.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import DropdownComponent from "../Dropdown/dropdown";
 import axios from "axios";
-
+import { API_ENDPOINT } from "../../App";
 import "./banker.css";
 
 function Banker() {
@@ -9,18 +9,17 @@ function Banker() {
 	const [loanData, setLoanData] = useState([]);
 	let currentStatus = "";
 
+	axios.defaults.headers.common["Authorization"] =
+		"eyJraWQiOiJrSURWK2lTS0RvMU5mMGJHbjhSUlpiMnNIdE1jYVJmS2JZMDB6eWVXM29rPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI5ZWIzYzZlNy1lMjM5LTRlZTQtYWUxOC1mMjNiNmU0NzhkN2UiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIHBob25lIG9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNjMzOTI2NTYzLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGgtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0aC0xX2hFamF6OGdxcyIsImV4cCI6MTYzNDAxMjk2MywiaWF0IjoxNjMzOTI2NTYzLCJ2ZXJzaW9uIjoyLCJqdGkiOiJlOWFjNTkwNC04ZTJiLTQ0NjktYWNmZC1kOTI2N2ZiMDdlYWIiLCJjbGllbnRfaWQiOiIzNHNvNGplcjQ0dTJwMzlmZmpxYmxuN2N2dCIsInVzZXJuYW1lIjoicHJhdmluIn0.tPTTc5uMkc2NEPo8mH6Zc5n2f_Qof9q0iUuhzI8ZW_Cucm-Ise7BpOQhrkCI1M_E71KOOqTyYk4r5NmVdq7HvRl9ZvWwfeioyqur5AjDhQ38x3is8dNkOpkbwrtg8MEdPnVKLYbRKfsiPWaiTw6YueBNkUZq8GX3yhpx-nMUSsM9p5fVRspahXB9BjZQx1IZcmYjxEY8VWoyWYB7c4W3QCMNK9uF0qUBKae93NWGzOle4YUqHSnW8skHy5CUkpuYwZINYBl42AKhkh3D_Q9qVNf3ANJQN0yMpEnp3Ri79R0g0vR9YWs8Jp3fxeHDeuL_EKZSnNl7HdL3a94ybBUS0g";
+
 	useEffect(() => {
 		axios
-			.get(
-				"https://g9yh14f7ve.execute-api.ap-south-1.amazonaws.com/Authorizeddev/getloanstatustypebanker/loanstatus"
-			)
+			.get(`${API_ENDPOINT}/getloanstatustypebanker/loanstatus`)
 			.then((res) => setStatus(res.data))
 			.catch((error) => console.log(error));
 
 		axios
-			.get(
-				"https://dc1nrv6pua.execute-api.ap-south-1.amazonaws.com/dev/loanapproval/banker"
-			)
+			.get(`${API_ENDPOINT}/creditapproval/creditor`)
 
 			.then((res) => setLoanData(res.data))
 			.catch((error) => console.log(error));
@@ -35,7 +34,7 @@ function Banker() {
 		await axios
 
 			.put(
-				`https://dc1nrv6pua.execute-api.ap-south-1.amazonaws.com/dev/creditapproval/creditor/${id}`,
+				`${API_ENDPOINT}/creditapproval/creditor/${id}`,
 
 				{
 					LoanApplication_Status: currentStatus,
