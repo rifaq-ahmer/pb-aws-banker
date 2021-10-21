071d94563ac88e498a3dc65d9a1fd3b0f0ed7e58
Rifaq.Shaikh <rifaq.shaikh@extentia.com>
2021-10-21 11:42:10 +0530

    completed styling

--- src/components/Dropdown/dropdown.js
+++ src/components/Dropdown/dropdown.js
@@ -9,22 +9,24 @@ function DropdownComponent({
 	value,
 }) {
 	return (
-		<form onSubmit={onSubmit} onChange={onChange} className="custom-dropdown">
-			<select
-				id={name}
-				name={name}
-				value={value || loanStatus}
-				disabled={loanStatus === 8}
-			>
-				{options.map((option) => (
-					<option key={option.status} value={option.status}>
-						{option.status_ID}
-					</option>
-				))}
-			</select>
+		<div>
+			<form onSubmit={onSubmit} onChange={onChange} className="custom-dropdown">
+				<select
+					id={name}
+					name={name}
+					value={value || loanStatus}
+					disabled={loanStatus === 8}
+				>
+					{options.map((option) => (
+						<option key={option.status} value={option.status}>
+							{option.status_ID}
+						</option>
+					))}
+				</select>
 
-			<input type="submit" value="Submit" />
-		</form>
+				<input type="submit" value="Submit" />
+			</form>
+		</div>
 	);
 }
 
